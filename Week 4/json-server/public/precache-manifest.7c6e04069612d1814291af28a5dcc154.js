84c1dae0232bf52c8771089a6a2fd1d123865926
Rohit Chawhan <rohitchawhan555@gmail.com>
2020-08-22 19:26:03 +0530

    Build and Deploy

---
+++ Week 4/json-server/public/precache-manifest.7c6e04069612d1814291af28a5dcc154.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d496a390f0018b8c3f6324910041d64f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf6de0e39d27db32a7f",
+    "url": "/static/css/2.4173b2d3.chunk.css"
+  },
+  {
+    "revision": "2e7099686cdae7a79577",
+    "url": "/static/css/main.a740c4be.chunk.css"
+  },
+  {
+    "revision": "fbf6de0e39d27db32a7f",
+    "url": "/static/js/2.0978e003.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.0978e003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7099686cdae7a79577",
+    "url": "/static/js/main.9508a31e.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
